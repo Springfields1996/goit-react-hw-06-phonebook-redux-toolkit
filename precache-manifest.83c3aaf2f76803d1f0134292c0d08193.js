8083f69d4ca39f65cbd1a824bf2520c77057a212
Bezzubov Artem <66363533+Springfields1996@users.noreply.github.com>
2020-09-23 19:07:30 +0300

    Updates

---
+++ precache-manifest.83c3aaf2f76803d1f0134292c0d08193.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eebaa785b5513625f883fc7e309bd53",
+    "url": "/goit-react-hw-06-phonebook-react-toolkit/index.html"
+  },
+  {
+    "revision": "ee5065991565b2910371",
+    "url": "/goit-react-hw-06-phonebook-react-toolkit/static/css/2.3d72dcb8.chunk.css"
+  },
+  {
+    "revision": "ce9270012bd9630a9a6d",
+    "url": "/goit-react-hw-06-phonebook-react-toolkit/static/css/main.530de835.chunk.css"
+  },
+  {
+    "revision": "ee5065991565b2910371",
+    "url": "/goit-react-hw-06-phonebook-react-toolkit/static/js/2.2702fdbf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-06-phonebook-react-toolkit/static/js/2.2702fdbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce9270012bd9630a9a6d",
+    "url": "/goit-react-hw-06-phonebook-react-toolkit/static/js/main.0a3f5682.chunk.js"
+  },
+  {
+    "revision": "a8e00b54e8bf65301bd7",
+    "url": "/goit-react-hw-06-phonebook-react-toolkit/static/js/runtime-main.9c35ced1.js"
+  }
+]);
